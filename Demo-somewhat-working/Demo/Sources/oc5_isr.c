bfee429a1c7fc09eb4ef82f33e5cbaba6da0d0d6
littlepetal <79516085+littlepetal@users.noreply.github.com>
2021-06-01 00:50:04 +1000

    WORKING integrated lidar, accelerometre and speaker modules

---
+++ Demo-somewhat-working/Demo/Sources/oc5_isr.c
@@ -0,0 +1,17 @@
+#include "derivative.h"
+#include "oc5_isr.h"
+
+extern volatile int dlycnt;
+
+// OC5 ISR //
+#pragma CODE_SEG __NEAR_SEG NON_BANKED /* Interrupt section for this module. Placement will be in NON_BANKED area. */
+__interrupt void OC5_ISR(void)  {
+  
+  
+  TC5 += dlycnt;
+  
+  //TFLG1_C5F = 1;
+  //TFLG1_C5F = 0;
+  
+  return; // return from interrupt
+}
